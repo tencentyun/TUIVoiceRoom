fe0091df325a0c192618adf4659390fab8164944
yuanfazheng <yuanfazheng@tencent.com>
2022-03-18 19:42:31 +0800

    MiniProgram】ADD LOG

--- MiniProgram/app.js
+++ MiniProgram/app.js
@@ -1,4 +1,5 @@
 // app.js
+import Aegis from './static/aegis'
 App({
   onLaunch() {
     this.globalData = {
@@ -9,5 +10,20 @@ App({
       },
       TUIScene: 'TUIVoiceRoom',
     };
+    this.aegisInit()
+    wx.aegis.reportEvent({
+      name: 'onLaunch',
+      ext1: 'onLaunch-success',
+      ext2: 'wxTUIVoiceRoomExternal',
+      ext3: genTestUserSig('').sdkAppID,
+    })
+  },
+  aegisInit() {
+    wx.aegis = new Aegis({
+      id: 'iHWefAYqxqlqtLQVcA', // 项目key
+      reportApiSpeed: true, // 接口测速
+      reportAssetSpeed: true, // 静态资源测速
+      pagePerformance: true, // 开启页面测速
+    });
   },
 });
